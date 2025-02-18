14221177f349e8199acd27970a43931430521eee
Hocuda <baam6t@gmail.com>
2025-02-19 00:30:16 +0200

    device interruptions in 2 seconds

---
+++ intr_tmp_file.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# displays device interruptions in 2 seconds
+
+TMPFILE1=$(mktemp /tmp/im1.XXXXXX)
+TMPFILE2=$(mktemp /tmp/im2.XXXXXX)
+cat /proc/interrupts > $TMPFILE1
+sleep 2
+
+cat /proc/interrupts > $TMPFILE2
+diff $TMPFILE1 $TMPFILE2
+rm -f $TMPFILE1 $TMPFILE2
